b57526ee1ed323fd2f607e3e0b3edf8152b73ad4
Nvnnain <mr3175647@gmail.com>
2024-05-19 17:07:45 +0530

    Update 0_Ice cream.js

--- Priyansh/commands/0_Ice cream.js
+++ Priyansh/commands/0_Ice cream.js
@@ -24,5 +24,5 @@ const fs = global.nodemodule["fs-extra"];
 "https://i.imgur.com/XCLiqfy.jpg",
      ];
      var callback = () => api.sendMessage({body:`─━━◉❖𝗝𝗨𝗟𝗠𝗜≛𝗝𝗔𝗔𝗧❖◉━━─\n❤️ ये लो बाबू आइस्क्रीम खाओ😋 -.-`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
-      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname + `/noprefix/Icecream.jpg")).on("close",() => callback());
    }
